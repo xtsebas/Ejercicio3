3c4a0130a572ce08b607a2e3cc83763e119b0067
Sebastian <thiagohugo2014@gmail.com>
2022-09-20 15:08:34 -0600

    cambios en la realizacion de escoger libros

--- src/Model/Controller.java
+++ src/Model/Controller.java
@@ -1,4 +1,26 @@
 package Model;
 
+import Functions.Choosething;
+import Functions.User;
+
+import java.util.Scanner;
+
 public class Controller {
+    public static User newuser(){
+        User user= new User();
+        return user;
+    }
+    public static Choosething newchoose(){
+        Choosething choosething= new Choosething();
+        return choosething;
+    }
+
+    public static void showUser(User user){
+        System.out.println("//////////////////////////////////////////////");
+        System.out.println("Nombre: " + user.getName());
+        System.out.println("Numero de identificacion: " + user.getIn());
+        System.out.println("Direccion: " + user.getDirection());
+        System.out.println("Cantidad de libros/revistas/articulos en prestamo: " + user.getAmountbooks());
+        System.out.println("//////////////////////////////////////////////");
+    }
 }
--- src/Model/DriverProgram.java
+++ src/Model/DriverProgram.java
@@ -1,4 +1,13 @@
 package Model;
 
+import Functions.Choosething;
+import Functions.User;
+
 public class DriverProgram {
+    public static User person;
+    public static Choosething choose;
+    public static void main(String[] args) {
+        person= Controller.newuser();
+        choose= Controller.newchoose();
+    }
 }
