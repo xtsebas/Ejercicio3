57fff3b51fff792043c3d398b7d1a8fda9a627b8
Sebastian <thiagohugo2014@gmail.com>
2022-09-21 08:32:09 -0600

    Para que digan que no es plagio, cambios en el driverprogram

--- src/Model/Controller.java
+++ src/Model/Controller.java
@@ -3,24 +3,71 @@ package Model;
 import Functions.Choosething;
 import Functions.User;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.SortedMap;
+
 
 public class Controller {
+    public static int Id;
+    static List<User> USER= new ArrayList<>();
+    static List<Choosething> things= new ArrayList<>();
+    static Scanner in= new Scanner(System.in);
     public static User newuser(){
         User user= new User();
+        USER.add(user);
+
         return user;
     }
     public static Choosething newchoose(){
         Choosething choosething= new Choosething();
+        things.add(choosething);
         return choosething;
     }
 
-    public static void showUser(User user){
-        System.out.println("//////////////////////////////////////////////");
-        System.out.println("Nombre: " + user.getName());
-        System.out.println("Numero de identificacion: " + user.getIn());
-        System.out.println("Direccion: " + user.getDirection());
-        System.out.println("Cantidad de libros/revistas/articulos en prestamo: " + user.getAmountbooks());
-        System.out.println("//////////////////////////////////////////////");
+    public static void showUser(){
+        for (int i = 0; i < USER.size(); i++) {
+            System.out.println(USER.get(i));
+        }
+    }
+    public static void searchID(){
+        System.out.println("Ingrese Id: ");
+        Id=in.nextInt();
+        in.nextLine();
+        for (Choosething choosething: things){
+            if (choosething.getId()== Id){
+                System.out.println("/////////////////////////////////////");
+                if (choosething.getThing()==1){
+                    System.out.println("El libro se ha encontrado");
+                    System.out.println("Titulo: " + choosething.getTitle());
+                    System.out.println("Id: " + choosething.getId());
+                    System.out.println("Disponibilidad: "+ choosething.getCondition());
+                } else if (choosething.getThing()==2) {
+                    System.out.println("La revista se ha encontrado");
+                    System.out.println("Titulo: " + choosething.getTitle());
+                    System.out.println("Id: " + choosething.getId());
+                    System.out.println("Disponibilidad: "+ choosething.getCondition());
+                } else if (choosething.getThing()==3) {
+                    System.out.println("El Articulo se ha encontrado");
+                    System.out.println("Titulo: " + choosething.getTitle());
+                    System.out.println("Id: " + choosething.getId());
+                    System.out.println("Disponibilidad: "+ choosething.getCondition());
+                }
+                System.out.println("/////////////////////////////////////");
+            }
+        }
+    }
+
+    public static void lend(){
+        for (Choosething choosething: things){
+            System.out.println("//////////////////////////////////////////////");
+            for (int i = 1; i < things.size(); i++) {
+                System.out.println(i + ". " + choosething.getTitle() + ": " + choosething.getCondition());
+            }
+            System.out.println(choosething.getTitle() + ": " + choosething.getCondition());
+            System.out.println("Cual desea?");
+
+        }
     }
 }
--- src/Model/DriverProgram.java
+++ src/Model/DriverProgram.java
@@ -3,11 +3,43 @@ package Model;
 import Functions.Choosething;
 import Functions.User;
 
+import java.util.Scanner;
+
 public class DriverProgram {
     public static User person;
     public static Choosething choose;
+    public static int op;
+    static Scanner scanner= new Scanner(System.in);
+    public static int opcion;
     public static void main(String[] args) {
-        person= Controller.newuser();
-        choose= Controller.newchoose();
+        op=1;
+        while (op==1){
+        System.out.println("BIENVENIDO A LA BIBLOTECA \nElija una opcion \n1. insertar libro/revista/articulo \n2. Buscar libro/revista/articulo por Id \n3. Realizar prestamo \n4. Devolver libro \n5. Nuevo usuario");
+        opcion=scanner.nextInt();
+            switch (opcion){
+                case 1:
+                    Controller.newchoose();
+                    break;
+                case 2:
+                    Controller.searchID();
+                    break;
+                case 3:
+                    Controller.lend();
+                    break;
+                case 4:
+
+                    break;
+                case 5:
+                    Controller.newuser();
+                    break;
+                case 6:
+                    System.out.println("Su opcion elegida no existe");
+                    break;
+            }
+            System.out.println("");
+            System.out.println("Desea realizar otra opcion? \n1. si \n2. no");
+            op=scanner.nextInt();
+            System.out.println("/////////////////////////////////////////////////////////////////");
+        }
     }
 }
